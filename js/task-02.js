ad694f73235f8cf66584f0f01f69f83b132eb8d0
Dmytro Holombytsia <iqbitsak@gmail.com>
2022-08-05 01:17:22 +0300

    complete task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const ingredientsListEl = document.querySelector('#ingredients')
+
+const makeIngredientElements = elements => {
+  return elements.map(item => {
+  const newElementList = document.createElement('li');
+  newElementList.textContent = item;
+  newElementList.classList.add('item');
+  return newElementList;
+    
+  })
+}
+
+const markup = makeIngredientElements(ingredients);
+ingredientsListEl.append(...markup);
