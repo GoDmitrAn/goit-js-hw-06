8bf3dd0da01268504551d18f5975ea58968c117a
Dmytro Holombytsia <iqbitsak@gmail.com>
2022-08-05 22:43:17 +0300

    complete task5

--- js/task-05.js
+++ js/task-05.js
@@ -1,6 +1,8 @@
-const inputEl = document.querySelector('#name-input');
-const spanEl = document.querySelector('#name-output');
+const inputEl = document.querySelector('input#name-input');
+const spanEl = document.querySelector('span#name-output');
 
 inputEl.addEventListener("input", (event) => {
+    const element = event.currentTarget;
+    element.value === '' ? spanEl.textContent = 'Anonymous' : spanEl.textContent = element.value;
     
 })
