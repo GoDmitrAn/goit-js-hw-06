ca7e4303e4197a87080352baaaa05c64adaa48d5
Dmytro Holombytsia <iqbitsak@gmail.com>
2022-08-07 01:51:28 +0300

    complete task10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,31 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const mainBlock = document.querySelector('#boxes');
+const inputEl = document.querySelector('#controls input');
+const btnCreateEl = document.querySelector('[data-create]');
+const btnDeleteEl = document.querySelector('[data-destroy]');
+
+let step = 30;
+function createBoxes(amount) {
+  for (let i = 0; i < amount; i += 1){
+    const newElementList = document.createElement('div');
+    newElementList.style.backgroundColor = getRandomHexColor();
+    newElementList.style.width = `${step}px`;
+    newElementList.style.height =`${step}px`;
+    
+    mainBlock.append(newElementList);
+    step += 10;
+  }
+};
+btnCreateEl.addEventListener('click', () => {
+  createBoxes(inputEl.value);
+});
+function destroyBoxes() {
+  inputEl.value = 0;
+  step = 30;
+  mainBlock.innerHTML='';
+}
+btnDeleteEl.addEventListener('click', () => {
+  destroyBoxes();
+});
