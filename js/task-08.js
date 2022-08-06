531e4203bd238d035e4eccde0159296a77a623e5
Dmytro Holombytsia <iqbitsak@gmail.com>
2022-08-06 22:42:38 +0300

    complete task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const formElement = document.querySelector('.login-form');
+
+formElement.addEventListener('submit', handleCheckForm);
+function handleCheckForm(event) {
+    event.preventDefault();
+    const {
+        elements: { email, password }
+    } = event.currentTarget;
+    if (email.value === '' || password.value === '') {
+        return alert("Please fill in all the fields!");
+    }
+    const userFormData = {
+        email: this.elements.email.value,
+        password: this.elements.password.value,
+    };
+    // console.log(`Login: ${email.value}, Password: ${password.value}`);
+    console.log("🚀 ~ userFormData", userFormData)
+
+  event.currentTarget.reset();
+}
 
