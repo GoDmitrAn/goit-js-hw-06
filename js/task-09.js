85f514368d1b14f704c5ad39eb36bf11f857fd00
Dmytro Holombytsia <iqbitsak@gmail.com>
2022-08-06 23:09:37 +0300

    complete task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,11 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const buttonElChangeColor = document.querySelector('.change-color');
+const spanElValue = document.querySelector('.color');
+const bodyEl=document.querySelector('body')
+
+buttonElChangeColor.addEventListener('click', () => {
+  spanElValue.textContent = getRandomHexColor();
+  bodyEl.style.backgroundColor = spanElValue.textContent;
+  })
